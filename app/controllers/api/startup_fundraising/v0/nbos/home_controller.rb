42c79a0c9942a3bc01631e905be1856a155282a8
sekhar <sekhar@nbostech.com>
2016-06-01 19:02:52 +0530

    Sending wordpress site as a link for about endpoint

--- app/controllers/api/startup_fundraising/v0/nbos/home_controller.rb
+++ app/controllers/api/startup_fundraising/v0/nbos/home_controller.rb
@@ -4,10 +4,10 @@ class Api::StartupFundraising::V0::Nbos::HomeController < Api::StartupFundraisin
 	 def about
 		 #tenantId = params[:tenant_id]
 		 #if tenantId.present?
-			 @about_50k = "<p>50k Network is a part of 50k Ventures, which is a network of high net worth individuals, accredited investors, and accomplished mentors.Diveded into different and quite diverse chapters, 50K Network caters to those who participate and contribute their share to the startup ecosystem in terms of funding, mentoring and providing strategic support for the growth and development of startups.</p>
-                     <p>To be a part of this much coveted circle, one has to meet the criteria.</p>
-                     <p>Right now, 50K Circle is spread actively in Hyderabad and Mumbai. However, we intend to spread our chapters all across the country soon.</p>"
-			 render :json => @about_50k
+			 #@about_50k = "<p>50k Network is a part of 50k Ventures, which is a network of high net worth individuals, accredited investors, and accomplished mentors.Diveded into different and quite diverse chapters, 50K Network caters to those who participate and contribute their share to the startup ecosystem in terms of funding, mentoring and providing strategic support for the growth and development of startups.</p>
+        #             <p>To be a part of this much coveted circle, one has to meet the criteria.</p>
+         #            <p>Right now, 50K Circle is spread actively in Hyderabad and Mumbai. However, we intend to spread our chapters all across the country soon.</p>"
+			 render :json =>  "http://50knetwork.com/about-us/"
 		 #else
 			# render :json => {status: 400, message: "Bad Request"}
 		 #end	
