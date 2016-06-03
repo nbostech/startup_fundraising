94a61dd80ac74c11602c4e9eef02b78fd09f5059
sekhar <sekhar@nbostech.com>
2016-06-03 17:01:27 +0530

    Adding funding round controller & customized the fundinground JSON response.

---
+++ app/controllers/api/startup_fundraising/v0/nbos/funding_rounds_controller.rb
@@ -0,0 +1,83 @@
+class Api::StartupFundraising::V0::Nbos::FundingRoundsController < Api::StartupFundraising::V0::StartupBaseController
+ 
+ before_action :validate_token
+ 
+  
+  def index
+    if params[:id].present? && @token_details.present? && @token_details.username.present?
+    	@company = Com::Nbos::StartupFundraising::Company.where(id: params[:id]).first
+    	if @company.present?
+    		render :json => @company.funding_rounds
+      else
+      	render :json => {status: 404, message: "Company Not Found"}
+      end		
+    else
+    	render :json => {status: 400, message: "Bad Request"}
+    end	
+  end
+
+  def create
+     if params[:id].present? && @token_details.present? && @token_details.username.present?
+       @company = Com::Nbos::StartupFundraising::Company.where(id: params[:id]).first
+       if @company.present? 
+         fundinground_params = params[:funding_round]
+         @funding_round = Com::Nbos::StartupFundraising::FundingRound.new(fundinground_params.permit!)
+	       @funding_round.company_id = @company.id
+	   
+	       if @funding_round.save
+	         render :json => @company.funding_rounds
+	       else
+	         render :json => {status: 500, message: @funding_round.errors.messages}, status: 500
+	       end
+       else
+       	 render :json => {status: 404, message: "Company Not Found"},status: 404
+       end   
+     else
+       render :json => {status: 400, message: "Bad Request"},status: 400
+     end   
+  end
+
+  def show
+    if params[:id].present?
+       @funding_round = Com::Nbos::StartupFundraising::FundingRound.where(id: params[:id]).first
+       if @funding_round.present?  
+	      render :json => @funding_round
+       else
+       	 render :json => {status: 404, message: "FundingRound Not Found"},status: 404
+       end   
+     else
+       render :json => {status: 400, message: "Bad Request"}, status: 400
+     end
+  end
+  
+  def update_fundingRound
+  	if params[:id].present? && @token_details.present? && @token_details.username.present?
+       @funding_round = Com::Nbos::StartupFundraising::FundingRound.where(id: params[:id]).first
+       funding_round_params = params[:funding_round]
+
+       if @funding_round.update_columns(funding_round_params.permit!) 
+	       render :json => @funding_round
+       else
+       	 render :json => {status: 404, message: "FundingRound Not Found"},status: 404
+       end   
+     else
+       render :json => {status: 400, message: "Bad Request"},status: 400
+     end
+  end
+
+  def delete
+  	if params[:id].present? && @token_details.present? && @token_details.username.present?
+       @funding_round = Com::Nbos::StartupFundraising::FundingRound.where(id: params[:id]).first
+       company_id = @funding_round.company_id if @funding_round.present?
+       if @funding_round.destroy 
+         @company = Com::Nbos::StartupFundraising::Company.where(id: company_id).first 
+	       render :json => @company.funding_rounds
+       else
+       	 render :json => {status: 404, message: "FundingRound Not Found"}, status: 404
+       end   
+     else
+       render :json => {status: 400, message: "Bad Request"},status: 400
+     end
+  end	
+
+end
--- app/models/com/nbos/startup_fundraising/funding_round.rb
+++ app/models/com/nbos/startup_fundraising/funding_round.rb
@@ -1,11 +1,15 @@
 module Com
-  module Nbos
-    module StartupFundraising
+	module Nbos
+		module StartupFundraising
 			class FundingRound < ActiveRecord::Base
 				belongs_to :company, class_name: "Com::Nbos::StartupFundraising::Company"
-        has_many :investments, class_name: "Com::Nbos::StartupFundraising::Investment"
-        belongs_to :funding_round_type, class_name: "Com::Nbos::StartupFundraising::FundingRoundType"
+				has_many :investments, class_name: "Com::Nbos::StartupFundraising::Investment"
+				belongs_to :funding_round_type, class_name: "Com::Nbos::StartupFundraising::FundingRoundType"
+			
+				def as_json(options={})
+				 super(:only => [:seeking_amount, :closing_date, :minimum_investment])
+				end
 			end
-	  end
+		end
 	end
 end		
