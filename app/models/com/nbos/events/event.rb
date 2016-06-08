e01df0e22c02a550abdba4dbb0c4264efffb891b
sekhar <sekhar@nbostech.com>
2016-06-08 20:13:48 +0530

    Fixing image url issue of events.

--- app/models/com/nbos/events/event.rb
+++ app/models/com/nbos/events/event.rb
@@ -23,7 +23,7 @@ module Com
 			 	  if Rails.env == "development"
 			 	  	host = "http://localhost:3000"
 			 	  else
-			 	  	host = "https://startup-50k.herokuapp.com"
+			 	  	host = "http://fundr.api.qa1.nbos.in"
 			 	  end 	
 			 	  host + image.url(:medium)
 			 end
