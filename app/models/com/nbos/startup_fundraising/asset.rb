d49cd400ab98ba344c32ea55459c01dcf68b9f6b
sekhar <sekhar@nbostech.com>
2016-06-28 15:08:47 +0530

    Added seperate subfolder for statup fundraising media storage.

--- app/models/com/nbos/startup_fundraising/asset.rb
+++ app/models/com/nbos/startup_fundraising/asset.rb
@@ -4,8 +4,8 @@ module Com
 			class Asset < ActiveRecord::Base
 			 belongs_to :imageable, polymorphic: true
        has_attached_file :image, :styles => { :small => "48x48",:medium => "300x200"},
-			                    :path => ":rails_root/public/images/media/:id/:style/:basename.:extension",
-			                    :url => "/images/media/:id/:style/:basename.:extension",
+			                    :path => ":rails_root/public/images/media/fund_raising/:id/:style/:basename.:extension",
+			                    :url => "/images/media/fund_raising/:id/:style/:basename.:extension",
 			                    :default_url => "/images/default/missing_image.png"
        validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 			 attr_accessor :delete_image 
